38e32b43192c9e268b468b47cd666b9a42d8b750
Dave Love <d.love@liverpool.ac.uk>
2011-01-03 16:39:26 +0000

    Changes from FreeBSD port. See http://www.freebsd.org/cgi/cvsweb.cgi/ports/sysutils/sge62/ Fixme:  Some of this may not be specific to FreeBSD, e.g. the fork_pty change in sge_pty.c.

--- source/clients/common/sge_client_ijs.c
+++ source/clients/common/sge_client_ijs.c
@@ -36,7 +36,7 @@
 #include <signal.h>
 #include <string.h>
 
-#if defined(DARWIN)
+#if defined(DARWIN) || defined(FREEBSD)
 #  include <termios.h>
 #  include <sys/ttycom.h>
 #  include <sys/ioctl.h>
--- source/libs/uti/sge_pty.c
+++ source/libs/uti/sge_pty.c
@@ -40,7 +40,7 @@
 #include <string.h>
 #include <pwd.h>
 
-#if defined(DARWIN) || defined(INTERIX)
+#if defined(DARWIN) || defined(FREEBSD) || defined(INTERIX)
 #  include <termios.h>
 #  include <sys/ioctl.h>
 #  include <grp.h>
@@ -58,6 +58,10 @@
 #  include <termios.h>
 #else
 #  include <termio.h>
+#endif
+
+#if defined(FREEBSD)
+#  include <libutil.h>
 #endif
 
 #include "sgermon.h"
@@ -127,7 +131,7 @@ int ptym_open(char *pts_name)
    }
    return -1;  /* out of pty devices */
 }
-#else
+#elif !defined(FREEBSD)
 int ptym_open(char *pts_name)
 {
    char *ptr;
@@ -207,7 +211,7 @@ int ptys_open(int fdm, char *pts_name)
    }
    return fds;
 }
-#else
+#elif !defined(FREEBSD)
 int ptys_open(int fdm, char *pts_name)
 {
    int      fds;
@@ -273,6 +277,31 @@ int ptys_open(int fdm, char *pts_name)
 *  SEE ALSO
 *     pty/fork_no_pty
 *******************************************************************************/
+#if defined(FREEBSD)
+pid_t fork_pty(int *ptrfdm, int *fd_pipe_err, dstring *err_msg)
+{
+   pid_t pid;
+   uid_t old_euid;
+
+   /* 
+    * We run this either as root with euid="sge admin user" or as an unprivileged 
+    * user.  If we are root with euid="sge admin user", we must change our
+    * euid back to root for this function.
+    */
+   old_euid = geteuid();
+   if (getuid() == SGE_SUPERUSER_UID) {
+      seteuid(SGE_SUPERUSER_UID);
+   }
+
+   if((pid = forkpty(*ptrfdm, NULL, NULL, NULL)) < 0) {
+      sge_dstring_sprintf(err_msg, "error in forkpty(): %d, %s",
+                          errno, strerror(errno));
+   }
+   seteuid(old_euid);
+
+   return pid;
+}
+#else
 pid_t fork_pty(int *ptrfdm, int *fd_pipe_err, dstring *err_msg)
 {
    pid_t pid;
@@ -366,6 +395,7 @@ pid_t fork_pty(int *ptrfdm, int *fd_pipe_err, dstring *err_msg)
       return pid;    /* parent returns pid of child */
    }
 }
+#endif
 
 /****** uti/pty/fork_no_pty() **************************************************
 *  NAME
