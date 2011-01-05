cc27c265ba60ecf8bbbae262922a2f3856b69419
Dave Love <d.love@liverpool.ac.uk>
2011-01-05 12:04:27 +0100

    It looks as if these changes from the latest FreeBSD ports source are
    Ignore-this: 4ad6602052b6d2a2afd1c5d175fc38b7
    actually earlier than the BSD bits currently in the source, e.g.
    <URL: http://gridengine.markmail.org/message/hirtywl5wir6faqa?q=freebsd#query:freebsd+page:1+mid:r47tp6q275s2yl4t+state:results>
    
    Rollback "Changes from FreeBSD port."
    
    rolling back:
    
    Mon Jan  3 16:39:26 GMT 2011  Dave Love <d.love@liverpool.ac.uk>
      * Changes from FreeBSD port.
      See http://www.freebsd.org/cgi/cvsweb.cgi/ports/sysutils/sge62/
      Fixme:  Some of this may not be specific to FreeBSD, e.g. the fork_pty
      change in sge_pty.c.
    
        M ./source/aimk -3 +5
        M ./source/aimk.private +3
        M ./source/clients/common/sge_client_ijs.c -1 +1
        M ./source/clients/qsh/qsh.c -1 +1
        M ./source/daemons/execd/execd_signal_queue.c -3 +3
        M ./source/daemons/shepherd/sge_shepherd_ijs.c -1 +1
        M ./source/daemons/shepherd/shepherd.c -1 +7
        M ./source/dist/pvm/src/aimk +7
        M ./source/dist/util/arch_variables +4
        M ./source/dist/util/create_settings.sh -1 +2
        M ./source/libs/uti/sge_pty.c -3 +33
        M ./source/scripts/distinst -1 +3
        M ./source/scripts/distinst.site -1 +1
    
    darcs-hash:a9639b0aee09a8f7ee313b20876b9eaac42ea90f

--- source/clients/common/sge_client_ijs.c
+++ source/clients/common/sge_client_ijs.c
@@ -36,7 +36,7 @@
 #include <signal.h>
 #include <string.h>
 
-#if defined(DARWIN) || defined(FREEBSD)
+#if defined(DARWIN)
 #  include <termios.h>
 #  include <sys/ttycom.h>
 #  include <sys/ioctl.h>
--- source/libs/uti/sge_pty.c
+++ source/libs/uti/sge_pty.c
@@ -40,7 +40,7 @@
 #include <string.h>
 #include <pwd.h>
 
-#if defined(DARWIN) || defined(FREEBSD) || defined(INTERIX)
+#if defined(DARWIN) || defined(INTERIX)
 #  include <termios.h>
 #  include <sys/ioctl.h>
 #  include <grp.h>
@@ -58,10 +58,6 @@
 #  include <termios.h>
 #else
 #  include <termio.h>
-#endif
-
-#if defined(FREEBSD)
-#  include <libutil.h>
 #endif
 
 #include "sgermon.h"
@@ -131,7 +127,7 @@ int ptym_open(char *pts_name)
    }
    return -1;  /* out of pty devices */
 }
-#elif !defined(FREEBSD)
+#else
 int ptym_open(char *pts_name)
 {
    char *ptr;
@@ -211,7 +207,7 @@ int ptys_open(int fdm, char *pts_name)
    }
    return fds;
 }
-#elif !defined(FREEBSD)
+#else
 int ptys_open(int fdm, char *pts_name)
 {
    int      fds;
@@ -277,31 +273,6 @@ int ptys_open(int fdm, char *pts_name)
 *  SEE ALSO
 *     pty/fork_no_pty
 *******************************************************************************/
-#if defined(FREEBSD)
-pid_t fork_pty(int *ptrfdm, int *fd_pipe_err, dstring *err_msg)
-{
-   pid_t pid;
-   uid_t old_euid;
-
-   /* 
-    * We run this either as root with euid="sge admin user" or as an unprivileged 
-    * user.  If we are root with euid="sge admin user", we must change our
-    * euid back to root for this function.
-    */
-   old_euid = geteuid();
-   if (getuid() == SGE_SUPERUSER_UID) {
-      seteuid(SGE_SUPERUSER_UID);
-   }
-
-   if((pid = forkpty(*ptrfdm, NULL, NULL, NULL)) < 0) {
-      sge_dstring_sprintf(err_msg, "error in forkpty(): %d, %s",
-                          errno, strerror(errno));
-   }
-   seteuid(old_euid);
-
-   return pid;
-}
-#else
 pid_t fork_pty(int *ptrfdm, int *fd_pipe_err, dstring *err_msg)
 {
    pid_t pid;
@@ -395,7 +366,6 @@ pid_t fork_pty(int *ptrfdm, int *fd_pipe_err, dstring *err_msg)
       return pid;    /* parent returns pid of child */
    }
 }
-#endif
 
 /****** uti/pty/fork_no_pty() **************************************************
 *  NAME
