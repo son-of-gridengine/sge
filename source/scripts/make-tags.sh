0cea9e676686d4380e2dab18ced48f2ecdfd05e2
Dave Love <d.love@liverpool.ac.uk>
2011-10-13 17:46:05 +0200

    Fix make-tags.sh
    Ignore-this: bec63e05436f56041c868f92e6a8ce5a
    
    darcs-hash:793295bd30ec0c45f0d4905bbb7d9fc99b731963

--- source/scripts/make-tags.sh
+++ source/scripts/make-tags.sh
@@ -4,4 +4,7 @@
 # and below it.
 
 # (Can't assume we have -print0.)
-find $(pwd) -name \*.c -o -name \*.h -o -name \*.java | xargs etags -a
+# Ignore the 3rparty stuff if run in source.
+rm -f TAGS
+find $(pwd) -name \*.c -o -name \*.h -o -name \*.java |
+  grep -v '#' | grep -v 3rdparty | xargs etags -a
