570aaa7162dd907620bee41d0b4c833244cc98d5
Dave Love <d.love@liverpool.ac.uk>
2012-06-18 13:42:39 +0200

    Don't convert \n (backslash+n) in environment variables to newline
    Ignore-this: e63592cfe9a4239bc04124d95a463997
    Abstract the unescaping.  Remove unused getenv_and_set.
    
    darcs-hash:5c5dbf9f408de870f5f83ee1aa055462e21767ab

--- source/libs/sgeobj/sge_var.c
+++ source/libs/sgeobj/sge_var.c
@@ -24,6 +24,7 @@
  *   The Initial Developer of the Original Code is: Sun Microsystems, Inc.
  * 
  *   Copyright: 2001 by Sun Microsystems, Inc.
+ *   Copyright (C) 2012 Dave Love, University of Liverpool
  * 
  *   All Rights Reserved.
  * 
@@ -112,6 +113,7 @@ const char *var_get_sharedlib_path_name(void)
 #elif defined(HP11)
    return "SHLIB_PATH";
 #elif defined(ALPHA) || defined(IRIX) || defined(LINUX) || defined(SOLARIS) || defined(FREEBSD) || defined(NETBSD) || defined(INTERIX) || defined(HP1164)
+   /* Fixme:  LD_LIBRARY_PATH_64 etc.?  */
    return "LD_LIBRARY_PATH";
 #elif defined(DARWIN)
    return "DYLD_LIBRARY_PATH";
@@ -305,6 +307,7 @@ void var_list_set_sharedlib_path(lList **varl)
    if(sharedlib_elem != NULL) {
       const char *old_value = lGetString(sharedlib_elem, VA_value);
 
+      /* fixme: looks wrong to add to it */
       if(old_value && strlen(old_value) > 0) {
          DPRINTF(("sharedlib path %s already set:\n", sharedlib_path_name));
          
@@ -344,6 +347,8 @@ void var_list_set_sharedlib_path(lList **varl)
 *     Dumps all list elements to <file> in the format:
 *     <name>=<value>
 *     <name> is read from VA_variable, <value> from VA_value.
+*     Any backslashes and newlines in <value> are backslash-escaped
+*     so that values may be printed on one line with un-escaping on read.
 *
 *  INPUTS
 *     varl - list of variables
@@ -360,18 +365,26 @@ void var_list_dump_to_file(const lList *varl, FILE *file)
    for_each(elem, varl) {
       /*
        * Replace <LF> by \n sequence for multiline environment
-       * variable support.
+       * variable support, after escaping backslash.
        */
       const char *env_name = lGetString(elem, VA_variable);
       const char *env_value = lGetString(elem, VA_value);
-      const char *new_env_value = sge_replace_substring(env_value, "\n", "\\n");
+      const char *new_env_value1, *new_env_value;
 
-      if (new_env_value == NULL) {
-         fprintf(file, "%s=%s\n", env_name, env_value);
-      } else {
-         fprintf(file, "%s=%s\n", env_name, new_env_value);
-         sge_free(&new_env_value);
+      if (!strchr (env_value, '\\')) {
+	fprintf(file, "%s=%s\n", env_name, env_value);
+	continue;
       }
+      /* Escape existing backslashes */
+      new_env_value1 = sge_replace_substring(env_value, "\\", "\\\\");
+      /* and then newlines */
+      new_env_value = sge_replace_substring((new_env_value1 ?
+					     new_env_value1 : env_value),
+					    "\n", "\\n");
+      fprintf(file, "%s=%s\n", env_name,
+              new_env_value ? new_env_value : new_env_value1);
+      sge_free(&new_env_value1);
+      sge_free(&new_env_value);
    }
 }
 
@@ -881,92 +894,3 @@ int var_list_parse_from_string(lList **lpp, const char *variable_str,
    sge_free(&str_str);
    DRETURN(0);
 }
-
-/****** sge_var/getenv_and_set() *******************************
-*  NAME
-*     getenv_and_set() -- obtain value for environment name, 
-*                         remove \n characters from value string
-*                         and set VA_Value member of *ep.
-*
-*  SYNOPSIS
-*     void getenv_and_set(lListElem *ep, char *variable)
-*
-*  FUNCTION
-*     Obtain value for environment name; check for any \n 
-*     characters in input string. Shell enters this character 
-*     for multi-line environment variables. If nothing found, 
-*     just store value in VA_Value member of *ep.
-*
-*     Otherwise, count number of newline characters, figure out
-*     string length and allocate new memory for the modified 
-*     string. Next, copy over string, skipping over \n characters.
-*     Then, store value in VA_Value member of *ep.
-*
-*  INPUTS
-*     lListElem *ep                  - target list element
-*     char *variable                 - environment variable
-*
-*  RESULT
-*     void
-*
-*  NOTES
-*     MT-NOTE: remove_newline_chars() is MT safe
-*******************************************************************************/
-
-void getenv_and_set(lListElem *ep, char *variable)
-{
-   const char *env_value = NULL;
-   char *new_env_value = NULL;
-   char *a = NULL;
-   char *b = NULL;
-   int i, nchars;
-   int newline_chars = 0;
-
-   env_value = sge_getenv(variable);
-   if (env_value == NULL) {
-      lSetString(ep, VA_value, NULL);
-      return;
-   }
-   /*
-    * Any work to do? Check for any newline
-    * character.
-    */
-   a = strchr(env_value, '\n');
-   if (a == NULL) {
-      /*
-       * Nothing to do. Just leave it alone.
-       */
-      lSetString(ep, VA_value, env_value);
-      return;
-   }
-   /*
-    * This is a multi-line environment variable. Allocate
-    * new string and copy over, but without newline chars.
-    */
-   nchars = strlen(env_value);
-   a = (char *)env_value;
-   newline_chars = 0;
-   for (i = 0; i < nchars; i++) {
-      if (*a == '\n') {
-         newline_chars++;
-      }
-      a++;
-   }
-   new_env_value = sge_malloc(nchars - newline_chars + 1);
-   a = new_env_value;
-   b = (char *)env_value;
-   for (i = 0; i < nchars; i++) {
-      if (*b != '\n') {
-         *a = *b;
-         a++;
-      }
-      b++;
-   }
-   *a = '\0';
-
-   lSetString(ep, VA_value, new_env_value);
-   sge_free(&new_env_value);
-   return;
-
-}
-
