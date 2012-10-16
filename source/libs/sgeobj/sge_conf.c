b1a88df79aaac5e6ecd8b22dc74654fcf8966cb7
Dave Love <d.love@liverpool.ac.uk>
2012-10-16 09:13:28 +0000

    Lower default MAX_DYN_EC somewhat Avoid a qmaster warning on typical Linux-based systems with "Max open files" 1024

--- source/libs/sgeobj/sge_conf.c
+++ source/libs/sgeobj/sge_conf.c
@@ -686,7 +686,7 @@ int merge_configuration(lList **answer_list, u_long32 progid, const char *cell_r
       prof_tevent_thrd = false;
       monitor_time = 0;
       scheduler_timeout = 0;
-      max_dynamic_event_clients = 1000;
+      max_dynamic_event_clients = 950;
       max_job_deletion_time = 3;
       enable_reschedule_kill = false;
       enable_reschedule_slave = false;
