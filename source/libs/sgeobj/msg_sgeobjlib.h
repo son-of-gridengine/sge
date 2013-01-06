9a9eaa3d9fb3d7364a155ed452c3e612e4463f9e
Dave Love <d.love@liverpool.ac.uk>
2013-01-06 12:46:01 +0000

    Fix #794 better and consider -tc too Refs #794

--- source/libs/sgeobj/msg_sgeobjlib.h
+++ source/libs/sgeobj/msg_sgeobjlib.h
@@ -319,8 +319,8 @@
 #define MSG_ATTR_INVALID_ULONGVALUE_USUU         _MESSAGE(64508, _(sge_U32CFormat" is an invalid value for the "SFQ" attribute - it must be a value between "sge_U32CFormat" and "sge_U32CFormat))
 #define MSG_CQUEUE_UNKNOWNSHELL_S                _MESSAGE(64509, _("Invalid value "SFQ" for shell "))
 #define MSG_CENTRY_NOTREQUESTABLE_S              _MESSAGE(64510, _("Complex attribute "SFQ" is not requestable"))
-#define MSG_ULNG_INVALIDPRIO_I                   _MESSAGE(64511, _("invalid priority %d.  Must be an integer from -1023 to 1024"))
-#define MSG_ULNG_INVALID_TASK_CONCURRENCY_I      _MESSAGE(64512, _("invalid task concurrency number %d. Must be an integer greater or equal to 0."))
+#define MSG_ULNG_INVALIDPRIO_S                   _MESSAGE(64511, _("invalid priority "SFQ".  Must be an integer from -1023 to 1024"))
+#define MSG_ULNG_INVALID_TASK_CONCURRENCY_S      _MESSAGE(64512, _("invalid task concurrency number "SFQ". Must be an integer greater or equal to 0."))
 #define MSG_ULNG_INVALID_VALUE                   _MESSAGE(64513, _("Invalid or not existing number."))
 
 #define MSG_JSV_EXISTS_S         _MESSAGE(64521, _("JSV file "SFQ" does not exist"))
