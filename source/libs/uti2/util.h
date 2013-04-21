46498657b95353145186b75e2aed33fde97f9613
Dave Love <d.love@liverpool.ac.uk>
2013-04-21 20:30:05 +0200

    Maybe implement strsignal and use it in qacct
    Ignore-this: 6079f27bf068e35e558f1f59f6cb429a
    
    darcs-hash:abde63d99298b687026548b7ec330bedf992c6c5

--- source/libs/uti2/util.h
+++ source/libs/uti2/util.h
@@ -2,5 +2,10 @@ bool is_dir(const char *dir);
 char *replace_char(char *str, size_t n, char c1, char c2);
 char * dev_file2string(const char* file, char *buffer, size_t *lbuffer);
 void sge_running_as_admin_user(bool *error, bool *isadmin);
-bool file_exists(char *file);
+bool file_exists(const char *file);
+/* fixme: extend, in the absence of autoconf */
+#define HAVE_STRSIGNAL __linux__ || __sun || __sun__ || __NetBSD__ || __FreeBSD__
+#if !HAVE_STRSIGNAL
+char *strsignal(int sig);
+#endif
 bool copy_linewise(const char *src, const char *dst);
