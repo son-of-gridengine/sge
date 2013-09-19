7ee81d3208cc610af8c46f7e8007ce58c87fba1d
Dave Love <d.love@liverpool.ac.uk>
2013-09-19 23:12:28 +0200

    Add extra help to start_gui_installer.sh
    Ignore-this: 26c86705fd18fce8966d8fd062583214
    
    darcs-hash:2aaad2d4e629d815dc1daff1510946613340f1e0

--- source/clients/gui-installer/templates/start_gui_installer.sh
+++ source/clients/gui-installer/templates/start_gui_installer.sh
@@ -33,13 +33,46 @@
 
 ErrUsage() 
 {
-   echo "Usage: `basename $0` [-help] [-resolve_pool=<num>] [-resolve_timeout=<sec>]"
-   echo "       [-install_pool=<num>] [-install_timeout=<sec>] [-connect_user=<usr>]"
-   echo "       [-connect_mode=windows]"
-   echo ""
-   echo "   <num> ... decimal number greater than zero"
-   echo "   <sec> ... number of seconds, must be greater then zero"
-   echo "   <usr> ... user name"
+   echo "\
+Usage: `basename $0` [-help] [-resolve_pool=<num>] [-resolve_timeout=<sec>]
+       [-install_pool=<num>] [-install_timeout=<sec>] [-connect_user=<usr>]
+       [-connect_mode=windows] [-debug]
+
+  -resolve_pool=<num>
+       Number of hosts that can be resolved in parallel when adding or
+       validating hosts or refreshing their states.  Higher values
+       produce a higher load during these operations.  Default 12.
+
+  -resolve_timeout=<sec>
+       Timeout applied to any resolve_pool operation.  Host validation
+       actually uses 2*resolve_timeout.  Increase this if hosts show an
+       Unreachable state and password-less access for the connect_user
+       is definitely working.  Default 20s.
+
+  -install_pool=<num>
+       Number of execution hosts that can be installed in parallel.
+       Higher values produce a higher load on the installation host.
+       Default 8.
+
+  -install_timeout=<sec>
+       Timeout for installation tasks. Increase it if tasks fail get
+       a Timeout state. Default 120s.
+
+  -connect_user=<usr>
+       User name for connecting to remote hosts.  Default is current user.
+
+  -connect_mode=windows
+       A host domain prefix is supplied for each connect_user.  Useful
+       when installing multiple MS Windows execution hosts which require
+       separate connect_users.
+
+  -debug
+       Prints voluminous debugging output.  Intended for development,
+       but may be more generally useful for diagnosing problems.
+
+   <num> ... decimal number greater than zero
+   <sec> ... number of seconds, must be greater then zero
+   <usr> ... user name"
    exit 1
 }
 
@@ -68,15 +101,15 @@ done
 
 #Detect JAVA
 . ./util/install_modules/inst_qmaster.sh
-HaveSuitableJavaBin "1.5.0" "none"
+# Assume building against OpenJDK6; fixme: does that actually matter?
+HaveSuitableJavaBin "1.6.0" "none"
 if [ $? -ne 0 ]; then
-   echo "Could not find Java 1.5 or later! Install Java from http://www.java.com/getjava and set your JAVA_HOME correcly or put java on your PATH!"
+   echo "Could not find Java 1.6 or later!
+Install Java from your operating system distribution or following
+http://openjdk.java.net/ and set your JAVA_HOME correctly or put java
+on your PATH!"
    exit 1
 fi
 
 echo "Starting Installer ..."
-if [ "$DEBUG_ENABLED" = "true" ]; then
-   $java_bin $FLAGS -jar ./util/gui-installer/installer.jar $ARGUMENTS
-else
-   $java_bin $FLAGS -jar ./util/gui-installer/installer.jar $ARGUMENTS
-fi
+$java_bin $FLAGS -jar ./util/gui-installer/installer.jar $ARGUMENTS
