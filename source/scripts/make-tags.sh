394acd6e3cf3f53dabc355f445a951d6b1a3ec0b
Dave Love <d.love@liverpool.ac.uk>
2013-09-29 20:34:40 +0200

    Put NAMEDEF, LISTDEF definitions in TAGS file
    Ignore-this: 3d3fb09da137ebba982dc36eb25fda10
    
    darcs-hash:cfccc4e498de69997b43a886360a710121501b27

--- source/scripts/make-tags.sh
+++ source/scripts/make-tags.sh
@@ -7,5 +7,5 @@
 # Ignore the 3rparty stuff if run in source.
 rm -f TAGS
 find common clients daemons libs security utilbin -name \*.[ch] -o -name \*.java |
-  xargs etags -a
+  xargs etags --regex='{c}/\(NAME\|LIST\)DEF\(([A-Za-z0-0_]+)\)/\2/' -a
 mkid -i C common clients daemons libs security utilbin
