b377322db1e7eb058518ceab8717c8f18f4cba50
Mark Dixon <m.c.dixon@leeds.ac.uk>
2013-12-09 17:42:04 +0100

    Added routine file_getvalue (retrieve values by key)
    Ignore-this: 2e5b4f257fd10483cc772d87106f22f6
    Search file for a given key, return a pointer to the rest of the line.
    
    darcs-hash:7322f975591e9e55bc7b411aab26d7b02041423a

--- source/libs/uti2/util.h
+++ source/libs/uti2/util.h
@@ -2,6 +2,7 @@
 #define __UTIL_H
 char *replace_char(char *str, size_t n, char c1, char c2);
 char * dev_file2string(const char* file, char *buffer, size_t *lbuffer);
+char *file_getvalue(char *buffer, int lbuffer, const char* file, const char *key);
 void sge_running_as_admin_user(bool *error, bool *isadmin);
 bool file_exists(const char *file);
 /* fixme: extend, in the absence of autoconf */
