4dcb96d8a03107aec0a9df900bacc338711be805
William Hay (AKA w.hay@ucl.ac.uk) <wish@dumain.com>
2015-05-16 16:01:48 +0200

    Fix #1536: Add appropriate macros, headers and storage classes to compile on MacOS X 10.9.5
    Ignore-this: 9a7699c966d3dcfc6e20d682d578ba19
    Also change the sed of DEPEND_FLAGS so that sge_depend doesn't complain about being unable to find
    the file x86_64.  The storage classes assigned to the variables in drmaa2_list_dict.h may not be
    correct but really not sure what the original coder though he was doing here as every file that
    includes that header was creating independent global variables with the same name AFAICT.
    
    darcs-hash:d05353b2e30e60fcb6f666c28f6a4faac1095362

--- source/utilbin/authuser.c
+++ source/utilbin/authuser.c
@@ -74,7 +74,7 @@ void sge_free(void *cp)
 
 #if defined(IRIX65) || defined(AIX43) || defined(HP1164) || defined(INTERIX) || defined(ALPHA5) || defined(WINDOWS) || defined(__OpenBSD__) || defined(__CYGWIN__)
 #define JUTI_NO_PAM
-#elif defined(DARWIN)
+#elif defined(DARWIN) && ! defined(SECURITYPAMAPPL)
 #include <pam/pam_appl.h>
 #else
 #include <security/pam_appl.h>
