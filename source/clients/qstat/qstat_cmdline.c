6e5e470c245b991db994b9d9ddbd719732e40e35
Dave Love <d.love@liverpool.ac.uk>
2015-10-31 15:43:48 +0000

    Message fixes

--- source/clients/qstat/qstat_cmdline.c
+++ source/clients/qstat/qstat_cmdline.c
@@ -313,14 +313,14 @@ qstat_usage(int qselect_mode, FILE *fp, char *what)
       fprintf(fp, "pe_list                  pe[,pe,...]\n");
       fprintf(fp, "job_identifier_list      [job_id|job_name|pattern]{, [job_id|job_name|pattern]}\n");
       fprintf(fp, "resource_list            resource[=value][,resource[=value],...]\n");
-      fprintf(fp, "user_list                user|@group[,user|@group],...]\n");
+      fprintf(fp, "user_list                user|*|@group[,user|*|@group],...]\n");
       fprintf(fp, "resource_attributes      resource,resource,...\n");
       fprintf(fp, "wc_cqueue                %s\n", MSG_QSTAT_HELP_WCCQ);
       fprintf(fp, "wc_host                  %s\n", MSG_QSTAT_HELP_WCHOST);
       fprintf(fp, "wc_hostgroup             %s\n", MSG_QSTAT_HELP_WCHG);
       fprintf(fp, "wc_qinstance             wc_cqueue@wc_host\n");
       fprintf(fp, "wc_qdomain               wc_cqueue@wc_hostgroup\n");
-      fprintf(fp, "wc_queue                 wc_cqueue|wc_qdomain|wc_qinstance\n");
+      fprintf(fp, "wc_queue_list            wc_cqueue|wc_qdomain|wc_qinstance\n");
       fprintf(fp, "wc_queue_list            wc_queue[,wc_queue,...]\n");
    } else {
       /* display option usage */
